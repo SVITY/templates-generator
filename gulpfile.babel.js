762880266674bcc7abbda35856fa05e3f705f754
Vitaly Solovyov <s.vitaly91@gmail.com>
2016-10-08 13:35:45 +0300

    add error notification when gulp gets error

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,15 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import chmod from 'gulp-chmod';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+
 
 gulp.task('default', () => {
   return gulp.src('src/index.js')
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>'),
+    }))
     .pipe(babel({
       presets: ['es2015', 'stage-0'],
     }))
