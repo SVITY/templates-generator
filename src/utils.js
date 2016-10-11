3b0fdac5d42a7fe34ef831e43867ff820c2ab997
Vitaly Solovyov <s.vitaly91@gmail.com>
2016-10-12 02:39:03 +0300

    add utils

---
+++ src/utils.js
@@ -0,0 +1,38 @@
+import readLineSync from 'readline-sync';
+// import replace from 'replace';
+
+export function getReplacements(words) {
+  const replacements = [];
+  for (const word of words) {
+    const replacement = readLineSync.question(`${word}: `);
+    const regexp = new RegExp(`###${word}###`, 'g');
+    replacements.push({
+      regexp,
+      replacement,
+    });
+  }
+  return replacements;
+}
+
+export function replaceInFile(file, replacements) {
+  let buffer = file;
+  for (const r of replacements) {
+    buffer = buffer.replace(r.regexp, r.replacement);
+  }
+  return buffer;
+}
+
+export function getNewFilesPaths(files, replacements, rootFilesPath) {
+  // current directory (where util runs)
+  const currentPath = process.cwd();
+  const newFiles = [];
+  for (const file of files) {
+    let newFile = file;
+    for (const r of replacements) {
+      newFile = newFile.replace(r.regexp, r.replacement);
+    }
+    newFile = newFile.replace(rootFilesPath, currentPath);
+    newFiles.push(newFile);
+  }
+  return newFiles;
+}
