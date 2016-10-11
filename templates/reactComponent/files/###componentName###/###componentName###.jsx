f021d73c83f4d65d56cb3997e8464c9b495cf882
Vitaly Solovyov <s.vitaly91@gmail.com>
2016-10-12 02:53:03 +0300

    add 'react component' template

---
+++ templates/reactComponent/files/###componentName###/###componentName###.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ###className###View from './###className###View';
+
+export default class ###className### extends Component {
+  static defaultProps = {
+  }
+
+  static propTypes = {
+  }
+
+  render() {
+    return (
+      <###className###View />
+    );
+  }
+}
---
+++ templates/reactComponent/files/###componentName###/###componentName###View.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ###componentName###View(props) {
+  return (
+    <div></div>
+  );
+}
+
+###componentName###View.propTypes = {
+};
